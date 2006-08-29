6f92a049e882d303fdfcabd3d868803ccac76c31
yselkowitz <yselkowitz@e127d73d-b18e-4ae1-a744-a22e6a3ccd30>
2006-08-29 23:58:10 +0000

    Version bump.
    git-svn-id: https://cygwin-ports.svn.sourceforge.net/svnroot/cygwin-ports/ports/trunk@337 e127d73d-b18e-4ae1-a744-a22e6a3ccd30

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,2 @@
-/usr/bin/gconftool-2 --direct --config-source=xml::/etc/gconf/gconf.xml.defaults --load /etc/gconf/schemas/panel-default-setup.entries ; \
-/usr/bin/gconftool-2 --direct --config-source=xml::/etc/gconf/gconf.xml.defaults --load /etc/gconf/schemas/panel-default-setup.entries /apps/panel ; \
+/usr/bin/gconftool-2 --direct --config-source=xml::/etc/gconf/gconf.xml.defaults --load /etc/gconf/schemas/panel-default-setup.entries
+/usr/bin/gconftool-2 --direct --config-source=xml::/etc/gconf/gconf.xml.defaults --load /etc/gconf/schemas/panel-default-setup.entries /apps/panel
